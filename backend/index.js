298687318edcbf37c9bc5f5861255ee5683265d5
ghimirepurnima09 <ghimirepurnima570@gmail.com>
2026-05-30 13:52:58 +0545

    changes in backend

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,19 @@
 import express from "express";
 import cors from "cors";
-
-import authRoutes from "./routes/authRoutes.js";
+import dotenv from "dotenv";
 import productRoutes from "./routes/productRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
+
+dotenv.config();
 
 const app = express();
 
 app.use(cors());
-
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
 
-app.use("/api/product", productRoutes);
-
-app.listen(5000, () => {
-    console.log("Server Running On 5000");
+app.listen(process.env.PORT, () => {
+    console.log(`Server Running On ${process.env.PORT}`);
 });
